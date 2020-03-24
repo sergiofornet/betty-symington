39cf16b761256c9a7a156a544f52973e0cca7fa6
sergiofornet <sergio.fornet@gmail.com>
2020-03-24 19:15:03 +0100

    Remove old files

--- main.js
+++
@@ -1,9 +0,0 @@
-function handleVisibilityChange() {
-	if (document.hidden) {
-		document.title = 'hello? 🕳️ I\'m still here...';
-	} else {
-		document.title = 'betty symington // frontend developer';
-	}
-}
-
-document.addEventListener("visibilitychange", handleVisibilityChange, false);
